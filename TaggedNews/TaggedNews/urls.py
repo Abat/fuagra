2ac3c889bddabc2d485beae654b541aa16eacec1
bagland <baglan.daribayev@gmail.com>
2015-02-05 23:44:14 -0500

    models setup

--- TaggedNews/TaggedNews/urls.py
+++ TaggedNews/TaggedNews/urls.py
@@ -5,6 +5,6 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'TaggedNews.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^main/', include('siteModel.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
