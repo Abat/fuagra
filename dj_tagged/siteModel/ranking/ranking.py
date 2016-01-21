744384ea5a56c736da3ff6127ac786d19e09a26c
Abat <ashakeno@gmail.com>
2016-01-21 18:16:35 -0500

    fixed code in ranking.py. Uncommented some code.

--- dj_tagged/siteModel/ranking/ranking.py
+++ dj_tagged/siteModel/ranking/ranking.py
@@ -37,7 +37,7 @@ class Ranking(object):
 		logger = logging.getLogger("django")
 		for news in news_list:
 			score = self._evaluate(news)
-			logger.info("Score of news " + str(score) + " " + news.title)
+			#logger.info("Score of news " + str(score) + " " + news.title)
 			news_dict[news] = score
 
 		#http://stackoverflow.com/questions/613183/sort-a-python-dictionary-by-value
@@ -45,8 +45,8 @@ class Ranking(object):
 		sorted_news_list_tuples = sorted(news_dict.items(), key=operator.itemgetter(1))
 		sorted_news_list = [item[0] for item in sorted_news_list_tuples]
 
-		for news in sorted_news_list:
-			logger.info("Sorted news " + news.title)
+		#for news in sorted_news_list:
+			#logger.info("Sorted news " + news.title)
 		return sorted_news_list
 
 #Composite of a weight + Ranking Algorithm
@@ -155,7 +155,7 @@ class WilsonScoreRankingAlgo(RankingAlgo):
 	def _evaluate_news(self, news):
                 logger = logging.getLogger("django")
                 logger.info("USing wilson rankning")
-		total_votes = news.upvotes + news.downvotes
+		total_votes = news.get_ups() + news.get_downs()
 		if (total_votes == 0):
 			return 0
 		z = 1.0 #1.0 = 85%, 1.6 = 95%
